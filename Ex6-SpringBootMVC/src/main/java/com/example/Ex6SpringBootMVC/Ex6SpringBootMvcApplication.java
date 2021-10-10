fecf33289bc43428da8f875903378fbfa45e87cd
magnus <578031@hvl.stud.no>
2021-10-10 16:31:40 +0200

    fin

---
+++ Ex6-SpringBootMVC/src/main/java/com/example/Ex6SpringBootMVC/Ex6SpringBootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ex6SpringBootMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex6SpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex6SpringBootMvcApplication.class, args);
+	}
+
+}
---
+++ Ex6-SpringBootMVC/src/test/java/com/example/Ex6SpringBootMVC/Ex6SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ex6SpringBootMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex6SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
